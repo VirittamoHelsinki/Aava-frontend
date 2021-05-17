4d41c801beed686ff54c9f6143aab0917542c93f
eync <1291215+eync@users.noreply.github.com>
2021-05-17 14:50:48 +0300

    quick fix

--- src/Routes.js
+++ src/Routes.js
@@ -9,7 +9,9 @@ import { Box } from '@material-ui/core';
 import CircularProgress from '@material-ui/core/CircularProgress';
 // const esimerkki = lazy(() => import('./esimerkki'));
 
-export default function Routes() {(
+export default function Routes() {
+  
+  return (
     <Router>
         <Suspense fallback={
           <Box variant="outlined" style={{minHeight:"100vh", display: "grid", placeContent: "center"}}><CircularProgress size="4rem" /></Box>
