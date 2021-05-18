944cdf61af27bd3debf5547891f8122804a3ff09
eync <1291215+eync@users.noreply.github.com>
2021-05-18 14:19:06 +0300

    Component testing page

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,8 @@ import {
     Route
 } from 'react-router-dom';
 
+import ComponentTest from './pages/ComponentTest';
+
 import { Box } from '@material-ui/core';
 import CircularProgress from '@material-ui/core/CircularProgress';
 // const esimerkki = lazy(() => import('./esimerkki'));
@@ -17,7 +19,9 @@ export default function Routes() {
           <Box variant="outlined" style={{minHeight:"100vh", display: "grid", placeContent: "center"}}><CircularProgress size="4rem" /></Box>
         }>
           <Switch>
-
+            <Route exact path="/test">
+              {ComponentTest}
+            </Route>
             {/* jos sivua ei löydy niin näytä tämä */}
             <Route path="*">
               {/* <PageNotFound /> */}
---
+++ src/pages/ComponentTest.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '../components/Button';
+import Container from '@material-ui/core/Container';
+
+export default
+    function ComponentTest(props) {
+        return (
+            <Container>
+                <h1>Komponenttien testaussivusto</h1>
+                <Button>test</Button>
+            </Container>
+        )
+    }
