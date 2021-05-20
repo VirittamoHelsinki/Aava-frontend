602ccb93f22a4e6182ead2a22267b9468c295f11
eync <eync@users.noreply.github.com>
2021-05-21 01:24:52 +0300

    PropTypes and clsx dependencies added

--- src/pages/ComponentTest.js
+++ src/pages/ComponentTest.js
@@ -11,11 +11,17 @@ export default
             <Container>
                 <h1>Komponenttien testaussivusto</h1>
                 <Paper>
-                <Button>test</Button>
-                <Button>test</Button>
-                <Button>test</Button>
-                <Button>test</Button>
-                test</Paper>
+                <Button look="login" color="primary">login button</Button>
+                <Button look="basic">basic button</Button>
+                <Button look="submit">submit button</Button>
+
+                <Button look="login" icon="AddButton" color="primary">icon button #1</Button>
+                <Button look="basic" icon="Settings">icon button #2</Button>
+                <Button look="submit" icon="PersonAdd">icon button #3</Button>
+                <Button look="submit" icon="PresentationAdd">icon button #4</Button>
+
+                <Button look="row" disabled>EI KÄYTÖSSÄ</Button>
+                </Paper>
                 <Chip>test</Chip>
                 <TextField></TextField>
             </Container>
