082d2eaaa774f18f36268ff298b5f2fd58a1fa32
Pietu123 <43340608+Pietu123@users.noreply.github.com>
2021-05-26 15:29:52 +0300

    chip edited

--- src/components/Chip/Chip.js
+++ src/components/Chip/Chip.js
@@ -3,7 +3,23 @@ import MUIchip from '@material-ui/core/Chip';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    
+    root: props => (
+        props.look === 'basic' ? {
+            backgroundColor:"#E9E9E9",
+            border: "2px solid #090909",
+            borderRadius: "12px",
+            margin:"1rem"
+                
+        } :  
+        props.look == 'secondary' ? {
+            color: "#000000",
+            textAlign: "left",
+            margin:"1rem"
+
+
+        }  : { /*none*/ }       
+
+    )
 }));
 
 export default 
@@ -11,9 +27,16 @@ export default
         const classes = useStyles();
         return (
             <MUIchip 
-                // className={classes.submit} 
+            className={props.look === "secondary" ? classes.secondary : classes.basic}
             >
                 {props.children || 'chippi'}
             </MUIchip>
         );
+}
+
+
+
+
+Chip.defaultProps = {
+    look: "basic"
 }
