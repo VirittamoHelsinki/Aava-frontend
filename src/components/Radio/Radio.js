86d1872650ce15991ddfa2a6f0a25a54f789eab2
Pietu123 <43340608+Pietu123@users.noreply.github.com>
2021-06-07 17:46:44 +0300

    radio edited

--- src/components/Radio/Radio.js
+++ src/components/Radio/Radio.js
@@ -4,18 +4,81 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import { withTheme } from '@material-ui/core/styles';
 
-export default 
-    function Select(props) {
-        return(
-            <FormControl component="fieldset">
-              <FormLabel component="legend">valitse</FormLabel>
-              <RadioGroup aria-label="valitse" name="valitse">
-                <FormControlLabel value="vaihtoehto 1" control={<Radio />} label="vaihtoehto 1" />
-                <FormControlLabel value="vaihtoehto 2" control={<Radio />} label="vaihtoehto 2" />
-                <FormControlLabel value="vaihtoehto 3" control={<Radio />} label="vaihtoehto 3" />
-                <FormControlLabel value="disabled" disabled control={<Radio />} label="(Disabled option)" />
-              </RadioGroup>
-            </FormControl>
-    );
+
+const useStyles = makeStyles({
+  root: {
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
+  },
+  icon: {
+    width: "24px",
+    height: "24px;",
+    background: "#E9E9E9 0% 0% no-repeat padding-box",
+    border: "2px solid #233550",
+    borderRadius: "6px",
+    opacity: "1",
+    '$root.Mui-focusVisible &': {
+      outline: '2px auto rgba(19,124,189,.6)',
+      outlineOffset: 2,
+    },
+    'input:disabled ~ &': {
+      boxShadow: 'none',
+      background: 'rgba(206,217,224,.5)',
+    },
+  },
+  checkedIcon: {
+    backgroundColor: "",
+    backgroundImage: "",
+    // '&:before': {
+    //   display: 'block',
+    //   width: 16,
+    //   height: 16,
+    //   backgroundImage: "",
+    //   content: '""',
+    // },
+    // 'input:hover ~ &': {
+
+    // },
+  },
+});
+
+
+function StyledRadio(props) {
+  const classes = useStyles();
+  return (
+    <Radio
+      className={classes.root}
+      disableRipple
+      color="default"
+      checkedIcon={<CheckBoxIcon  color={props.theme.black}/>}
+      icon={<span className={classes.icon} />}
+      {...props}
+    />
+  );
+}
+
+const RadioButton = withTheme(StyledRadio); 
+
+export default
+  function Select(props) {
+
+  return (
+
+    <FormControl component="fieldset">
+      <FormLabel component="legend">valitse</FormLabel>
+      <RadioGroup aria-label="valitse" name="valitse"  >
+        <FormControlLabel value="vaihtoehto 1" control={<RadioButton />} label="vaihtoehto 1" />
+        <FormControlLabel value="vaihtoehto 2" control={<RadioButton />} label="vaihtoehto 2" />
+        <FormControlLabel value="vaihtoehto 3" control={<RadioButton />} label="vaihtoehto 3" />
+        <FormControlLabel value="disabled" disabled control={<RadioButton />} label="(Disabled option)" />
+
+      </RadioGroup>
+    </FormControl>
+  );
 }
