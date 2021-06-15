45697b1ab45e578a8ea0cdde8258ba807e7806af
Pietu123 <43340608+Pietu123@users.noreply.github.com>
2021-06-15 13:14:33 +0300

    datafetch urls added

--- src/components/DataFetch/DataFetch.js
+++ src/components/DataFetch/DataFetch.js
@@ -12,11 +12,11 @@ export default
         // checks out if url parameter matches to anything and outputs it
         function getParameterFetch(fetchaddress) {
             return {
-                dashboard: "", usermanagement: "", presentation: "",
-                technology: "", employee: "", project: "",
-                attachment: "", user: "", projectdeveloper: "",
-                createproject: "", createprojectdeveloper: "",
-                employeeforpresentation: ""
+                dashboard: "127.0.0.1:8000/dashboard", usermanagement: "127.0.0.1:8000/usermanagement", presentation: "127.0.0.1:8000/presentation",
+                technology: "127.0.0.1:8000/technology", employee: "127.0.0.1:8000/employee", project: "127.0.0.1:8000/project",
+                attachment: "127.0.0.1:8000/attachment", user: "127.0.0.1:8000/user", projectdeveloper: "127.0.0.1:8000/projectdeveloper",
+                createproject: "127.0.0.1:8000/createproject", createprojectdeveloper: "127.0.0.1:8000/createprojectdeveloper",
+                employeeforpresentation: "127.0.0.1:8000/employeeforpresentation"
             }[fetchaddress]
         }
         
