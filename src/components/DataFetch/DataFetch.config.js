7e8b51e72768019e1c7b17511c9aa42d9b6ab37b
eync <1291215+eync@users.noreply.github.com>
2021-06-22 09:39:35 +0300

    code splitting for better usability between components

---
+++ src/components/DataFetch/DataFetch.config.js
@@ -0,0 +1,10 @@
+/*
+*
+* configuration as object for other sibling components
+*
+*/
+
+export 
+    const config = {
+        baseUrl: "http://localhost:8000"
+    }
---
+++ src/components/DataFetch/ParameterFetch.js
@@ -0,0 +1,18 @@
+/* 
+*
+* Checks the param and if it matches then returns it.
+* @param {string} fetchaddress    parameter for checking.
+* @return {string} fetchaddress   when parameter matches, then outputs the found one.
+*
+*/
+
+export default 
+    function getParameterFetch(fetchaddress) {
+        return {
+            dashboard: "dashboard", usermanagement: "usermanagement", presentation: "presentation",
+            technology: "technology", employee: "employee", project: "project",
+            attachment: "attachment", user: "user", projectdeveloper: "projectdeveloper",
+            createproject: "createproject", createprojectdeveloper: "createprojectdeveloper",
+            employeeforpresentation: "employeeforpresentation"
+        }[fetchaddress]
+    }
