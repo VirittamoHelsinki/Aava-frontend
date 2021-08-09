28f39cc1e69d442d7af618a2c47607470e2bda94
Noora Peltokangas <noora.peltok@gmail.com>
2021-08-09 08:54:42 +0300

    loginpage almost done

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,12 +5,24 @@ import Grid from '@material-ui/core/Grid';
 import { Button } from '../components/Button';
 import { TextField } from '../components/TextField';
 import { Paper } from '../components/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import { Checkbox } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  paper: {
+      marginTop: 100,
+      paddingTop: 30,
+      height: 400,
+  },
+  Checkbox: {
+    marginLeft: 113,
+  }
+})
 // @ts-check
 
 export default
     function LoginPage(props) {
-        const TextFieldWidth = {width: '400px'}
+        const TextFieldWidth = {width: '300px'}
         const [login, setLogin] = useState(false);
         const [username, setUsername] = useState([]);
         const [password, setPassword] = useState([]);
@@ -29,19 +41,25 @@ export default
             return username.length > 0 && password.length > 0;
         }
 
+        const classes = useStyles()
+
         // check if not logged in and then show login page, if logged in then redirect.
         if (!login) {
             return (
-                <Container component={Paper} maxWidth="sm">
+                <Container component={Paper} maxWidth="sm" classes={{root: classes.paper}}>
                     <Grid>
                         <form>
                             <Grid item xs={12} style={{display:"grid", justifyItems:"center"}}>
-                                <h1>Kirjaudu Sisään</h1>
+                                <h1>Kirjaudu sisään</h1>
                             </Grid>    
                             <Grid item xs style={{display:"grid", justifyItems:"center"}}>
                                 <TextField label="username" style={TextFieldWidth} value={username} onChange={e => setUsername(e.target.value)} />
                                 <TextField label="password" type="password" style={TextFieldWidth} value={password} onChange={e => setPassword(e.target.value)} />
-                            </Grid> 
+                            </Grid>
+                            <grid>
+                                <Checkbox classes={{root: classes.Checkbox}}></Checkbox>
+                                <text>Muista minut</text>
+                            </grid>    
                             <Grid item xs={12} style={{display:"grid", justifyItems:"center"}}>
                                 <Button look="login" onClick={() => setLogin(true)} disabled={!validateInputs()}>kirjaudu</Button>
                             </Grid>
